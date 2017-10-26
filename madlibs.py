ff7d2a1bc56bf8efcaf089305e98ffad0fac8239
Edward Ahn <edward.ahn@edwardahn.local>
2017-10-26 13:42:58 -0400

    10/26

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,36 @@
 #! python3
 # madlibs.py - reads in text files and lets the user add their own text
 # anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file
+
+import re
+
+# define regex
+verbRegex = re.compile(r'verb', re.I)
+nounRegex = re.compile(r'noun', re.I)
+adjectiveRegex = re.compile(r'adjective', re.I)
+
+textfile = open('madlibs.txt')
+
+content = textfile.read()
+textfile.close()
+
+index = 0
+newMadLib = []
+
+for word in content.split():
+    newMadLib.append(word)
+    if verbRegex.search(word) != None:
+        x = input('Enter VERB:  ')
+        newMadLib[index] = x
+    elif nounRegex.search(word) != None:
+        x = input('Enter NOUN:  ')
+        newMadLib[index] = x
+    elif adjectiveRegex.search(word) != None:
+        x = input('Enter ADJECTIVE:  ')
+        newMadLib[index] = x
+    index += 1
+
+newMadLib = (' ').join(newMadLib)
+
+print(content)
+print (newMadLib)
